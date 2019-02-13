cd1a5c79db3d637926a7862501616dfd527b3859
DenisPavlov <denispavlov1989@gmail.com>
2019-02-13 13:00:09 +1000

    javascript

--- js/examples/alert.js
+++ js/examples/alert.js
@@ -1,5 +1,16 @@
-var user = {};
-user.name = 'Вася';
-user.surname = 'Петров';
-user[name] = 'Сергей';
-delete user.name;
+function multiplyNumeric(obj) {
+    for (var key in obj) {
+        if (!isNaN(obj[key])) {
+            obj[key] = obj[key] * 2;
+        }
+    }
+    return obj;
+}
+
+var menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+alert(multiplyNumeric(menu).toString())
