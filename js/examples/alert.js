047b2dd8280f54cf6543432526ed50652b4167f0
DenisPavlov <denispavlov1989@gmail.com>
2019-02-22 12:47:27 +1000

    js

--- js/examples/alert.js
+++ js/examples/alert.js
@@ -1,9 +1,23 @@
-var styles = ['Джаз','Блюз'];
-alert(styles)
-styles.push('Рок-н-Ролл');
-alert(styles)
-styles[styles.length-2] = 'Классика';
-alert(styles)
-alert(styles.shift());
-styles.unshift('Рэп', 'Регги');
-alert(styles)
+var obj = {
+    className: 'open menu'
+};
+
+function addClass(obj, cls) {
+    var array = obj.className.split(" ");
+    
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === cls) return;
+
+        if (i === array.length-1) array.push(cls)
+    }
+
+    obj.className = array.join(" ");
+
+    array.filter()
+}
+
+
+addClass(obj, 'new'); // obj.className='open menu new'
+addClass(obj, 'open'); // без изменений (класс уже существует)
+addClass(obj, 'me'); // obj.className='open menu new me'
+alert( obj.className ); // "open menu new me"
