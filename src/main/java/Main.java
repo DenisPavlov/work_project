1c3247a69061f930eead87468acf68e2814b23d2
DenisPavlov <denispavlov1989@gmail.com>
2019-04-18 17:09:37 +1000

    decompile

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    private String hh = "HHH";
+
+    public static void main(String[] args) {
+
+//        String s2 = "Hello";
+        String s1 = new String("Hello");
+//        s1.intern();
+
+//        String s = "Hello";
+
+//        for (int i = 0; i < 1000; i++) {
+//            String ss = "Hello" + i;
+//            String sss = "Hello" + i;
+//            System.out.println(ss == sss);
+//        }
+
+//        System.out.println(s == s1);
+
+
+//        String ss = "Hello" + 1;
+//        String sss = "Hello" + 1;
+//        System.out.println(ss == sss);
+    }
+}
