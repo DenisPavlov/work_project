527e2bf5c8190f1889fdb9bbd5857f9fca69e331
denis <denispavlov1989@gmail.com>
2019-11-29 21:42:46 +0700

    book

--- src/main/java/books/horstman/v1ch13/resource/ResourceTest.java
+++ src/main/java/books/horstman/v1ch13/resource/ResourceTest.java
@@ -1,4 +1,4 @@
-package resource;
+package books.horstman.v1ch13.resource;
 
 import java.awt.*;
 import java.io.*;
@@ -7,44 +7,39 @@ import java.util.*;
 import javax.swing.*;
 
 /**
- * @version 1.41 2015-06-12
  * @author Cay Horstmann
+ * @version 1.41 2015-06-12
  */
-public class ResourceTest
-{
-   public static void main(String[] args)
-   {
-      EventQueue.invokeLater(() -> {
-         JFrame frame = new ResourceTestFrame();
-         frame.setTitle("ResourceTest");
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         frame.setVisible(true);
-      });
-   }
+public class ResourceTest {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new ResourceTestFrame();
+            frame.setTitle("ResourceTest");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
 }
 
 /**
  * A frame that loads image and text resources.
  */
-class ResourceTestFrame extends JFrame
-{
-   private static final int DEFAULT_WIDTH = 300;
-   private static final int DEFAULT_HEIGHT = 300;
+class ResourceTestFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 300;
 
-   public ResourceTestFrame()
-   {
-      setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-      URL aboutURL = getClass().getResource("about.gif");
-      Image img = new ImageIcon(aboutURL).getImage();
-      setIconImage(img);
+    public ResourceTestFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        URL aboutURL = getClass().getResource("about.gif");
+        Image img = new ImageIcon(aboutURL).getImage();
+        setIconImage(img);
 
-      JTextArea textArea = new JTextArea();
-      InputStream stream = getClass().getResourceAsStream("about.txt");
-      try (Scanner in = new Scanner(stream, "UTF-8"))
-      {
-         while (in.hasNext())
-            textArea.append(in.nextLine() + "\n");
-      }
-      add(textArea);
-   }
+        JTextArea textArea = new JTextArea();
+        InputStream stream = getClass().getResourceAsStream("about.txt");
+        try (Scanner in = new Scanner(stream, "UTF-8")) {
+            while (in.hasNext())
+                textArea.append(in.nextLine() + "\n");
+        }
+        add(textArea);
+    }
 }
