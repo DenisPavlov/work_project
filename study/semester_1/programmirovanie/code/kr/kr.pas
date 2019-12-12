6fe9f7d13fee892cd3bb2f8a88c3de58a144415b
denis <denispavlov1989@gmail.com>
2019-12-12 13:37:33 +0700

    study

--- study/semester_1/programmirovanie/code/kr/kr.pas
+++ study/semester_1/programmirovanie/code/kr/kr.pas
@@ -1,14 +1,20 @@
-{ Сформировать двумерный массив A размером N x 2 (N - количество строк, 
-* равное предпоследней цифре пароля (у меня 1) + 2; 2 – количество столбцов) с 
-* помощью генератора случайных чисел и вывести элементы массива на экран
-* и в файл. Найти наибольший элемент каждой строки матрицы А (оформить 
-* нахождение максимального элемента в виде функции). Из этих 
-* максимальных элементов составить одномерный массив F. Вывести элементы
-*  массива F на экран и в файл.}
 
 var A: array [1..3,1..2] of integer;
     i,j: integer;
     f : text;
+    maxElements: array[1..3] of integer;
+
+function maxElement (var x: array of integer): integer;
+var i, max: integer;
+begin
+ max:=x[0];
+ for i:=1 to high(x) do 
+   begin
+     if (x[i] > max) then max:=x[i]
+   end;
+ maxElement:=max;
+end;
+
 
 begin
   assign(f,'kr.txt');
@@ -26,53 +32,22 @@ begin
         end;
     writeln;
     writeln(f);
+    
+    maxElements[i]:=maxElement(A[i]);
   end;
+  
+  writeln();
+  writeln(f);
+  writeln('Максимальные элементы строк');
+  writeln(f, 'Максимальные элементы строк');
+  for i:=1 to 3 do
+    begin
+      write(maxElements[i]:4);
+      write(f, maxElements[i]:4);
+    end;
+
+  writeln();
+  writeln(f);   
+  
   close(f);
 end. 
-
-
-{varm: array [1..5,1..10] of integer;
-
-  i,j,s, min: integer;
-
-begin
-
-  randomize;
-
-  for i:=1 to 5 do
-
-  begin
-
-    for j:=1 to 10 do
-
-    begin
-
-        m[i,j]:=random(100);
-
-        write(m[i,j]:4);
-
-    end;
-
-    writeln;
-
-  end;
-
-  s:=0; min:=m[1,1];
-
-  for i:=1 to 5 do
-
-    for j:=1 to 10 do
-
-    begin
-
-        s:=s+m[i,j];
-
-        if m[i,j]<min then min:=m[i,j];
-
-    end;
-
-  writeln(‘S = ’,s,’ min = ’,min);
-
-end.
-}
- 
