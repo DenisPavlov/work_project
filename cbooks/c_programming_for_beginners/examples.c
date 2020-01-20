940346da46b6f36a500eac9eb1d62358a2d18137
denis <denispavlov1989@gmail.com>
2020-01-20 22:59:21 +0700

    study

---
+++ cbooks/c_programming_for_beginners/examples.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+main() {
+    printf("размер int %d байт \n", sizeof(int));
+    printf("размер long %d байт \n", sizeof(long));
+    printf("размер char %d байт \n", sizeof(char));
+}
